9f47a8740dff7254ae30a147055fe8545686963c
idris <idriswakil123@gmail.com>
2023-03-16 04:48:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+/**
+ *main - A program that print line using printf
+ *Return: 0 (success)
+ */
+int main(void)
+{
+prinf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
